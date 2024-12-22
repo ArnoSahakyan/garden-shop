cc9dca60b9054cbe37a5c16a98e7eae43adbf473
ArnoSahakyan <sahakyan.arno@gmail.com>
2024-12-22 13:53:26 +0400

    Added navigation feature to Title.jsx

--- src/components/sale/Sale.jsx
+++ src/components/sale/Sale.jsx
@@ -1,6 +1,7 @@
 import './Sale.scss'
 import Title from "../title/Title.jsx";
 import ProductCard from "../productCard/ProductCard.jsx";
+import {useNavigate} from "react-router-dom";
 
 const sales = [
     {title: "Decorative forged bridge", discount: 50, initialPrice: 1000, salePrice: 500, img: "/product1.png"},
@@ -10,11 +11,14 @@ const sales = [
 ];
 
 export default function Sale() {
+    const navigate = useNavigate();
+
     return (
         <section className="Sale container">
             <Title
                 title="Sale"
                 subtitle="All sales"
+                onClick={() => navigate('/sales')}
             />
             <div className="Sale__cards">
                 {
