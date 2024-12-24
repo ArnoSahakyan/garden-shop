8deed4e1acb638b5e4f0a90b20bbce3a2e47f194
emo366 <73887722+Emo366@users.noreply.github.com>
2024-12-25 02:08:14 +0400

    add-cart

--- src/pages/cart/Cart_Page.jsx
+++ src/pages/cart/Cart_Page.jsx
@@ -3,15 +3,19 @@ import {useState} from "react";
 import Modal from "../../components/modal/Modal.jsx";
 import Title from "../../components/title/Title.jsx";
 import {useNavigate} from "react-router-dom";
+import { useSelector } from 'react-redux';
 
 export default function Cart_Page() {
     const navigate = useNavigate();
     const [isOpen, setIsOpen] = useState(false);
+    const cartList=useSelector((state)=>state.products.cart)
     const toggleModal = () => setIsOpen(!isOpen);
+
 
     return (
         <div className='Cart container'>
             <Title title="Shopping cart" subtitle="Back to store" onClick={() => navigate('/')}/>
+             
             {
                 isOpen &&
                 <Modal
--- src/pages/product/Product_Page.jsx
+++ src/pages/product/Product_Page.jsx
@@ -1,97 +1,91 @@
 import './Product_Page.scss';
-import {useParams} from "react-router-dom";
-import {selectProductById} from "../../store/products/productsSlice.js";
-import {useSelector} from "react-redux";
-import ButtonCard from "../../components/buttonCard/ButtonCard.jsx";
-import {useState} from "react";
+import { useParams } from 'react-router-dom';
+import { selectProductById } from '../../store/products/productsSlice.js';
+import { useDispatch, useSelector } from 'react-redux';
+import ButtonCard from '../../components/buttonCard/ButtonCard.jsx';
+import { useState } from 'react';
+import { addCart } from '../../store/products/productsSlice.js';
 
 export default function Product_Page() {
-    const {id} = useParams();
-    const product = useSelector(selectProductById(Number(id)));
-    const {title, description, initialPrice, discount, img} = product;
-    
-    const [isExpanded, setIsExpanded] = useState(false);
-    const [count, setCount] = useState(0);
-    const [cart, setCart] = useState([]);
+	const { id } = useParams();
+	const product = useSelector(selectProductById(Number(id)));
+	const { title, description, initialPrice, discount, img } = product;
+   
 
-    const salePrice = (price, discount) => {
-        return Math.ceil(price * (100 - discount) / 100);
-    };
+	const dispatch = useDispatch();
 
-    const toggleReadMore = () => {
-        setIsExpanded(!isExpanded);
-    };
+	const [isExpanded, setIsExpanded] = useState(false);
+	const [count, setCount] = useState(0);
 
-    const handleIncrement = () => {
-        setCount(count + 1);
-    };
+	const salePrice = (price, discount) => {
+		return Math.ceil((price * (100 - discount)) / 100);
+	};
 
-    const handleDecrement = () => {
-        if (count > 0) {
-            setCount(count - 1)
-        }
-    };
+	const toggleReadMore = () => {
+		setIsExpanded(!isExpanded);
+	};
 
+	const handleIncrement = () => {
+		setCount(count + 1);
+	};
 
-    const addCart = () => {
-        if (count === 0) return; 
-        const productToAdd = {
-            ...product,
-            quantity: count,
-            totalPrice: salePrice(initialPrice, discount) * count, 
-        };
+	const handleDecrement = () => {
+		if (count > 0) {
+			setCount(count - 1);
+		}
+	};
 
-        setCart(prevCart => {
-            const index = prevCart.findIndex(item => item.id === productToAdd.id);
-            if (index !== -1) {
-         
-                const updatedCart = [...prevCart];
-                updatedCart[index].quantity += productToAdd.quantity;
-                updatedCart[index].totalPrice = salePrice(initialPrice, discount) * updatedCart[index].quantity;
-                return updatedCart;
-            } else {
-        
-                return [...prevCart, productToAdd];
-            }
-        });
-
-        setCount(0);
-    };
-
-    return (
-        <div className="ProductPage container">
-            <div className="ProductPage__img">
-                <img src={img} alt={title} />
-            </div>
-            <div className="ProductPage__info">
-                <h2>{title}</h2>
-                <div className="price">
-                    <h2>${discount ? salePrice(initialPrice, discount) : initialPrice}</h2>
-                    {discount && <h5>${initialPrice}</h5>}
-                    {discount && (
-                        <div className="discount">
-                            <p>-{discount}%</p>
-                        </div>
-                    )}
-                </div>
-                <div className="action">
-                    <div className='counter'>
-                        <button className='decrement' onClick={handleDecrement}>-</button>
-                        <span>{count}</span>
-                        <button className='increment' onClick={handleIncrement}>+</button>
-                    </div>
-                    <div className="btn">
-                        <ButtonCard title="Add to cart" onClick={addCart} />
-                    </div>
-                </div>
-                <h4>Description</h4>
-                <p className={`description ${isExpanded ? 'expanded' : ''}`}>
-                    {description}
-                </p>
-                <a className="read-more" onClick={toggleReadMore}>
-                    {isExpanded ? 'Read Less' : 'Read More'}
-                </a>
-            </div>
-        </div>
-    );
+	const handleAddCart = () => {
+		if (count === 0) return;
+		const productToAdd = {
+			...product,
+			quantity: count,
+			totalPrice: salePrice(initialPrice, discount) * count,
+		};
+     
+        const { description, ...filteredProduct } = productToAdd;
+        dispatch(addCart(filteredProduct));
+	};
+	return (
+		<div className="ProductPage container">
+			<div className="ProductPage__img">
+				<img src={img} alt={title} />
+			</div>
+			<div className="ProductPage__info">
+				<h2>{title}</h2>
+				<div className="price">
+					<h2>
+						${discount ? salePrice(initialPrice, discount) : initialPrice}
+					</h2>
+					{discount && <h5>${initialPrice}</h5>}
+					{discount && (
+						<div className="discount">
+							<p>-{discount}%</p>
+						</div>
+					)}
+				</div>
+				<div className="action">
+					<div className="counter">
+						<button className="decrement" onClick={handleDecrement}>
+							-
+						</button>
+						<span>{count}</span>
+						<button className="increment" onClick={handleIncrement}>
+							+
+						</button>
+					</div>
+					<div className="btn">
+						<ButtonCard title="Add to cart" onClick={handleAddCart} />
+					</div>
+				</div>
+				<h4>Description</h4>
+				<p className={`description ${isExpanded ? 'expanded' : ''}`}>
+					{description}
+				</p>
+				<a className="read-more" onClick={toggleReadMore}>
+					{isExpanded ? 'Read Less' : 'Read More'}
+				</a>
+			</div>
+		</div>
+	);
 }
